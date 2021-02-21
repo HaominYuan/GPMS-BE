22e37bf728fdc1572fab9cca36a4416c435039bc
tstxxy <tstxxy@gmail.com>
2021-02-21 20:33:41 +0800

    change

--- src/main/java/tstxxy/gra/MainVerticle.java
+++ src/main/java/tstxxy/gra/MainVerticle.java
@@ -1,33 +1,31 @@
 package tstxxy.gra;
 
+import com.sun.tools.jconsole.JConsoleContext;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
 import io.vertx.core.json.JsonObject;
+import io.vertx.ext.auth.JWTOptions;
+import io.vertx.ext.auth.PubSecKeyOptions;
+import io.vertx.ext.auth.jwt.JWTAuth;
+import io.vertx.ext.auth.jwt.JWTAuthOptions;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+
 
 public class MainVerticle extends AbstractVerticle {
-    Router router;
-    String login = "tstxxy";
-    String password = "qwer1234";
-    String identity = "teacher";
+
+    private JWTAuth provider;
 
     @Override
     public void start(Promise<Void> startPromise) throws Exception {
-        router = Router.router(vertx);
+        provider = JWTAuth.create(vertx, new JWTAuthOptions()
+            .addPubSecKey(new PubSecKeyOptions()
+                .setAlgorithm("HS256")
+                .setBuffer("keyboard cat")));
 
-        router.get("/login").handler(ctx -> {
-            var response = ctx.response();
-            response.setChunked(true);
+        Router router = Router.router(vertx);
 
-            var params = ctx.request().params();
-            if (params.get(("username")).equals(login) && params.get("password").equals(password)) {
-                ctx.json(new JsonObject().put("state", "Yes"));
-            } else {
-                ctx.json(new JsonObject().put("state", "No"));
-            }
-            response.end();
-
-        });
+        router.get("/login").handler(this::loginHandler);
 
         vertx.createHttpServer().requestHandler(router).listen(80, http -> {
             if (http.succeeded()) {
@@ -37,5 +35,28 @@ public class MainVerticle extends AbstractVerticle {
                 startPromise.fail(http.cause());
             }
         });
+
+    }
+
+    private void loginHandler(RoutingContext ctx) {
+        var response = ctx.response();
+        response.setChunked(true);
+
+        var params = ctx.request().params();
+        String username = params.get("username");
+        String password = params.get("password");
+        System.out.println(username);
+        System.out.println(password);
+        if ("tstxxy".equals(username) && "qwer1234".equals(password)) {
+            ctx.json(new JsonObject().put("state", "Yes"));
+            String token = provider.generateToken(new JsonObject().put("username", username),
+                new JWTOptions().setExpiresInSeconds(120));
+            ctx.response().putHeader("Authorization", token);
+        } else {
+            ctx.json(new JsonObject().put("state", "No"));
+        }
+
+
+        response.end();
     }
 }

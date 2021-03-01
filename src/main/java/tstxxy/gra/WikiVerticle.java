add6e0b89e8786aae96c6ba9a7658136968bef8a
tstxxy <tstxxy@gmail.com>
2021-03-01 22:40:26 +0800

    commit

--- src/main/java/tstxxy/gra/WikiVerticle.java
+++ src/main/java/tstxxy/gra/WikiVerticle.java
@@ -14,6 +14,8 @@ import io.vertx.ext.web.templ.freemarker.FreeMarkerTemplateEngine;
 import io.vertx.pgclient.PgConnectOptions;
 import io.vertx.pgclient.PgPool;
 import io.vertx.sqlclient.PoolOptions;
+import io.vertx.sqlclient.Row;
+import io.vertx.sqlclient.RowSet;
 import io.vertx.sqlclient.Tuple;
 
 import java.util.*;
@@ -23,8 +25,10 @@ import java.util.stream.Collectors;
 public class WikiVerticle extends AbstractVerticle {
     private static final String SQL_CREATE_PAGES_TABLE = "create table if not exists Pages (Id serial primary key, Name varchar(255) unique, Content text);";
     private static final String SQL_GET_PAGE = "select Id, Content from Pages where Name=$1"; // <1>
-    private static final String SQL_CREATE_PAGE = "insert into Pages values (NULL, ?, ?)";
-    private static final String SQL_SAVE_PAGE = "update Pages set Content = ? where Id = ?";
+
+    private static final String SQL_CREATE_PAGE = "insert into Pages values (default, $1, $2)";
+    private static final String SQL_SAVE_PAGE = "update Pages set Content = $1 where Id = $2";
+
     private static final String SQL_ALL_PAGES = "select Name from Pages";
     private static final String SQL_DELETE_PAGE = "delete from Pages where id=$1";
     private static final String EMPTY_PAGE_MARKDOWN =
@@ -52,27 +56,17 @@ public class WikiVerticle extends AbstractVerticle {
         Promise<Void> promise = Promise.promise();
         dbClient = PgPool.pool(vertx, connectOptions, poolOptions);
 
-        dbClient.getConnection(ar -> {
-            if (ar.failed()) {
-                LOGGER.error("Could not open a database connection", ar.cause());
-                promise.fail(ar.cause());
-            } else {
-                var connection = ar.result();
-                connection.query(SQL_CREATE_PAGES_TABLE).execute().onComplete(create -> {
-                    if (ar.failed()) {
-                        LOGGER.error("Database preparation error", create.cause());
-                        promise.fail(create.cause());
-                    } else {
-                        promise.complete();
-                    }
-                });
-            }
+        dbClient.getConnection().compose(conn -> conn.query(SQL_CREATE_PAGES_TABLE)
+            .execute().eventually(v -> conn.close())
+        ).onFailure(e -> {
+            LOGGER.error(e.getCause());
+            promise.fail(e.getCause());
+        }).onSuccess(result -> {
+            promise.complete();
         });
-
 
         return promise.future();
     }
-
 
     private Future<Void> startHttpServer() {
         Promise<Void> promise = Promise.promise();
@@ -82,8 +76,8 @@ public class WikiVerticle extends AbstractVerticle {
         router.get("/").handler(this::indexHandler);
         router.get("/wiki/:page").handler(this::pageRenderingHandler);
         router.post().handler(BodyHandler.create());
-//        router.post("/save").handler(this::pageUpdateHandler);
-//        router.post("/create").handler(this::pageCreateHandler);
+        router.post("/save").handler(this::pageUpdateHandler);
+        router.post("/create").handler(this::pageCreateHandler);
         router.post("/delete").handler(this::pageDeletionHandler);
 
         templateEngine = FreeMarkerTemplateEngine.create(vertx);
@@ -104,37 +98,31 @@ public class WikiVerticle extends AbstractVerticle {
 
     private void indexHandler(RoutingContext context) {
         dbClient.getConnection().compose(conn -> conn.query(SQL_ALL_PAGES)
-            .execute().onComplete(res -> {
-                if (res.succeeded()) {
-                    var it = res.result().iterator();
-                    final List<String> pages = new ArrayList<>();
-                    res.result().forEach(row -> {
-                        pages.add(row.getString("name"));
+            .execute().onSuccess(rs -> {
+                var it = rs.iterator();
+                final List<String> pages = new ArrayList<>();
+                rs.forEach(row -> {
+                    pages.add(row.getString("name"));
+                });
 
-                    });
+                context.put("title", "Wiki home")
+                    .put("pages", pages.stream().sorted().collect(Collectors.toList()));
 
-                    context.put("title", "Wiki home");
-                    context.put("pages", pages.stream().sorted().collect(Collectors.toList()));
+                templateEngine.render(context.data(), "templates/index.ftl").onSuccess(buffer -> {
+                    context.response().putHeader("Context-Type", "text/html").end(buffer);
+                }).onFailure(e -> {
+                    LOGGER.error(e.getMessage());
+                    context.fail(e.getCause());
+                });
 
-                    templateEngine.render(context.data(), "templates/index.ftl").onComplete(tar -> {
-                        if (tar.succeeded()) {
-                            context.response().putHeader("Context-Type", "text/html");
-                            context.response().end(tar.result());
-                        } else {
-                            context.fail(tar.cause());
-                            LOGGER.error(tar.cause());
-                        }
-                    });
-                } else {
-                    LOGGER.error(res.cause());
-                    context.fail(res.cause());
-                }
 
+            }).onFailure(e -> {
+                LOGGER.error(e.getMessage());
+                context.fail(e.getCause());
             }).eventually(v -> conn.close())
-        ).onComplete(ar -> {
-            if (ar.succeeded()) return;
-            LOGGER.error(ar.cause());
-            context.fail(ar.cause());
+        ).onFailure(e -> {
+            LOGGER.error(e.getMessage());
+            context.fail(e.getCause());
         });
 
     }
@@ -142,20 +130,15 @@ public class WikiVerticle extends AbstractVerticle {
     private void pageDeletionHandler(RoutingContext context) {
         String id = context.request().getParam("id");
         dbClient.getConnection().compose(conn -> conn.preparedQuery(SQL_DELETE_PAGE)
-            .execute(Tuple.of(Integer.parseInt(id))).onComplete(res -> {
-                if (res.succeeded()) {
-                    context.response().setStatusCode(303);
-                    context.response().putHeader("Location", "/");
-                    context.response().end();
-                } else {
-                    LOGGER.error(res.cause());
-                    context.fail(res.cause());
-                }
+            .execute(Tuple.of(Integer.parseInt(id)))
+            .onSuccess(v -> context.response().setStatusCode(303).putHeader("Location", "/").end())
+            .onFailure(e -> {
+                LOGGER.error(e.getMessage());
+                context.fail(e.getCause());
             }).eventually(v -> conn.close())
-        ).onComplete(ar -> {
-            if (ar.succeeded()) return;
-            LOGGER.error(ar.cause());
-            context.fail(ar.cause());
+        ).onFailure(e -> {
+            LOGGER.error(e.getMessage());
+            context.fail(e.getCause());
         });
     }
 
@@ -163,47 +146,80 @@ public class WikiVerticle extends AbstractVerticle {
         String page = context.request().getParam("page");
 
         dbClient.getConnection().compose(conn -> conn.preparedQuery(SQL_GET_PAGE)
-            .execute(Tuple.of(Tuple.of(page))).onComplete(res -> {
-                if (res.succeeded()) {
-                    var it = res.result().iterator();
+            .execute(Tuple.of(page)).onSuccess(rs -> {
+                var it = rs.iterator();
 
+                context.put("title", page)
+                    .put("timestamp", new Date().toString());
 
-                    context.put("title", page);
-                    context.put("timestamp", new Date().toString());
+                if (it.hasNext()) {
+                    var row = it.next();
+                    var id = row.getInteger(0);
+                    var rawContent = row.getString(1);
 
-                    if (it.hasNext()) {
-                        var row = it.next();
-                        var id = row.getInteger(0);
-                        var rawContent = row.getString(1);
-
-                        context.put("id", id);
-                        context.put("newPage", "no");
-                        context.put("rawContent", rawContent);
-                        context.put("content", Processor.process(rawContent));
-
-                    } else {
-                        context.put("id", -1);
-                        context.put("newPage", "yes");
-                        context.put("rawContent", EMPTY_PAGE_MARKDOWN);
-                        context.put("content", Processor.process(EMPTY_PAGE_MARKDOWN));
-                    }
-
-
-                    templateEngine.render(context.data(), "templates/page.ftl", ar -> {
-                        if (ar.succeeded()) {
-                            context.response().putHeader("Content-Type", "text/html");
-                            context.response().end(ar.result());
-                        } else {
-                            LOGGER.error(ar.cause());
-                            context.fail(ar.cause());
-                        }
-                    });
+                    context.put("id", id)
+                        .put("newPage", "no")
+                        .put("rawContent", rawContent)
+                        .put("content", Processor.process(rawContent));
                 } else {
-                    LOGGER.error(res.cause());
-                    context.fail(res.cause());
+                    context.put("id", -1)
+                        .put("newPage", "yes")
+                        .put("rawContent", EMPTY_PAGE_MARKDOWN)
+                        .put("content", Processor.process(EMPTY_PAGE_MARKDOWN));
                 }
-            }).eventually(v -> conn.close())
-        );
 
+                templateEngine.render(context.data(), "templates/page.ftl").onSuccess(buffer -> {
+                    context.response().putHeader("Content-Type", "text/html").end(buffer);
+                }).onFailure(e -> {
+                    LOGGER.error(e.getMessage());
+                    context.fail(e.getCause());
+                });
+            }).onFailure(e -> {
+                LOGGER.error(e.getCause());
+                context.fail(e.getCause());
+            }).eventually(v -> conn.close())
+        ).onFailure(e -> {
+            LOGGER.error(e.getCause());
+            context.fail(e.getCause());
+        });
+    }
+
+    private void pageUpdateHandler(RoutingContext context) {
+        Integer id = Integer.valueOf(context.request().getParam("id"));
+        String title = context.request().getParam("title");
+        String markDown = context.request().getParam("markdown");
+        boolean newPage = "yes".equals(context.request().getParam("newPage"));
+
+        dbClient.getConnection().compose(conn -> {
+            Future<RowSet<Row>> result;
+            if (newPage) {
+                result = conn.preparedQuery(SQL_CREATE_PAGE).execute(Tuple.of(title, markDown));
+            } else {
+                result = conn.preparedQuery(SQL_SAVE_PAGE).execute(Tuple.of(markDown, id));
+            }
+
+            return result.onSuccess(rs -> {
+                context.response().setStatusCode(303)
+                    .putHeader("Location", "/wiki/" + title)
+                    .end();
+            }).onFailure(e -> {
+                LOGGER.error(e.getMessage());
+                context.fail(e.getCause());
+            }).eventually(v -> conn.close());
+        }).onFailure(e -> {
+            LOGGER.error(e.getMessage());
+            context.fail(e.getCause());
+        });
+    }
+
+    private void pageCreateHandler(RoutingContext context) {
+        String pageName = context.request().getParam("name");
+        String location = "/wiki/" + pageName;
+        if (pageName == null || pageName.isEmpty()) {
+            location = "/";
+        }
+        context.response().setStatusCode(303)
+            .putHeader("Location", location)
+            .end();
     }
 }
